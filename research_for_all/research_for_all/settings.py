81e6b9b73b0adf18b7e265153e1a473d32831e17
elisagemart <elisagemart@gmail.com>
2021-02-26 21:52:51 -0500

    Connected DB!

--- research_for_all/research_for_all/settings.py
+++ research_for_all/research_for_all/settings.py
@@ -73,11 +73,16 @@ WSGI_APPLICATION = 'research_for_all.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'rfa',
+    'USER': 'admin',
+    'PASSWORD': 'rfapass**',
+    'HOST': 'rfa-db.cqqb5ivlygcu.us-east-2.rds.amazonaws.com',
+    'PORT': '3306',
+   }
 }
 
 
