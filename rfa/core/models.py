60cc8377748ddec8f0a7b281887bb7622dbd29c7
Sarwar Hussain <sarwar.hussain@yale.edu>
2021-03-05 17:23:45 -0500

    Created 'position' and 'affiliation' fields for signup

--- rfa/core/models.py
+++ rfa/core/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
 class CustomUser(AbstractUser):
+    """ Adding the two extra attributes of a user, their affiliation (ex: Yale) and position (ex: Student, Prof.) here. 
+    For now these fields are allowed to be blank (blank=True)."""
     affiliation = models.CharField(blank=True, max_length=120)
+    position = models.CharField(blank=True, max_length=120)
--- rfa/frontend/src/components/signup.js
+++ rfa/frontend/src/components/signup.js
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 import axiosInstance from "../axiosApi";
 
 
+
 class Signup extends Component{
     constructor(props){
         super(props);
         this.state = {
+            firstname: "",
+            lastname: "",
+            affiliation: "",
+            position: "",
             username: "",
-            password: "",
             email:"",
+            password: "",
             errors:""
         };
 
@@ -20,10 +25,15 @@ class Signup extends Component{
         this.setState({[event.target.name]: event.target.value});
     }
 
+    //('first_name', 'last_name', 'affiliation', 'position', 'email', 'username', 'password')
     async handleSubmit(event) {
         event.preventDefault();
         try {
             const response = await axiosInstance.post('/user/create/', {
+                first_name: this.state.firstname,
+                last_name: this.state.lastname,
+                affiliation: this.state.affiliation,
+                position: this.state.position,
                 username: this.state.username,
                 email: this.state.email,
                 password: this.state.password
@@ -42,6 +52,26 @@ class Signup extends Component{
             <div>
                 Signup
                 <form onSubmit={this.handleSubmit}>
+                    <label>
+                        First Name:
+                        <input name="firstname" type="text" value={this.state.firstname} onChange={this.handleChange}/>
+                        { this.state.errors.firstname ? this.state.errors.firstname : null}
+                    </label>
+                    <label>
+                        Last Name:
+                        <input name="lastname" type="text" value={this.state.lastname} onChange={this.handleChange}/>
+                        { this.state.errors.lastname ? this.state.errors.lastname : null}
+                    </label>
+                    <label>
+                        Affiliation:
+                        <input name="affiliation" type="text" value={this.state.affiliation} onChange={this.handleChange}/>
+                        { this.state.errors.affiliation ? this.state.errors.affiliation : null}
+                    </label>
+                    <label>
+                        Position:
+                        <input name="position" type="text" value={this.state.position} onChange={this.handleChange}/>
+                        { this.state.errors.position ? this.state.errors.position : null}
+                    </label>
                     <label>
                         Username:
                         <input name="username" type="text" value={this.state.username} onChange={this.handleChange}/>
