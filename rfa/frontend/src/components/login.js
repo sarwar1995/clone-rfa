79d2428180e5f1e41f9e2f92964c29d55940713f
Sarwar Hussain <sarwar.hussain@yale.edu>
2021-03-05 17:28:30 -0500

    Fixed bug in login.js, now returns 'response'

--- rfa/frontend/src/components/login.js
+++ rfa/frontend/src/components/login.js
@@ -24,7 +24,7 @@ class Login extends Component {
             axiosInstance.defaults.headers['Authorization'] = "JWT " + response.data.access;
             localStorage.setItem('access_token', response.data.access);
             localStorage.setItem('refresh_token', response.data.refresh);
-            return data;
+            return response;
         } catch (error) {
             throw error;
         }
