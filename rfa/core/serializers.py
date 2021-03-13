b772d2e563c95c8f087c94f829d394c95b0fbffc
Sarwar Hussain <sarwar.hussain@yale.edu>
2021-03-13 15:03:28 -0500

    Fixed bug in validate_<field_name> that broke the code when database is empty

--- rfa/core/serializers.py
+++ rfa/core/serializers.py
@@ -2,6 +2,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from .models import CustomUser
 from django.core.validators import EmailValidator
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -10,7 +11,11 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super(MyTokenObtainPairSerializer, cls).get_token(user)
 
-        # Add custom claims. We don't need affilation as a custom claim to verify the user. Only username and password will suffice
+        # Add custom claims.
+        #token['affiliation'] = user.affiliation
+        # We don't need affilation as a custom claim to verify the user.
+        # Only username and password will suffice which is part of the parent class TokenObtainPairSerializer
+        # So in principle we don't need this child class if there are no custom claims
         return token
 
 
@@ -32,14 +37,14 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     def validate_username(self, value):
         """
-        Check that the username is not already taken by searching for it in the database via model.objects. 
+        Check that the username is not already taken by searching for it in the database via model.objects.get(). 
         If a user with that username is found then raise a validation error.
         """
         try:
             user = self.Meta.model.objects.get(username=value)
             raise serializers.ValidationError(
                 "username is already taken. Please enter a different username")
-        except User.DoesNotExist:
+        except ObjectDoesNotExist:
             return value
 
     def validate_email(self, value):
@@ -51,7 +56,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
             user = self.Meta.model.objects.get(email=value)
             raise serializers.ValidationError(
                 "email is already taken please enter a different email")
-        except user.DoesNotExist:
+        except ObjectDoesNotExist:
             validate_email = EmailValidator()
             validate_email(value)
             return value
@@ -59,8 +64,8 @@ class CustomUserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """
         This create method is only called when "user = serializer.save()" is called in views.py after validating the serializer
-        via the validate_email and validate_username methods have already been called. And if serializer aka data in
-        the form is valid then the create method checks for password and if not None saves an instance of the model
+        via the validate_email and validate_username methods have already been called. And if serializer (aka data in
+        the form) is valid then this create method checks for password and if not None saves an instance of the model
         (aka user) in the databse.
         """
         password = validated_data.pop('password', None)
