8b141194546f5c16178a35e92e058b958dcc96cf
elisagemart <elisagemart@gmail.com>
2021-03-20 16:26:58 -0400

    integrated endpoints with frontend

--- rfa/rfa/urls.py
+++ rfa/rfa/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('search/', include('search.urls')),
-    path('papers/', include('papers.urls')),
     path('admin/', admin.site.urls),
+    path('api/search/', include('search.urls')),
+    path('api/papers/', include('papers.urls')),
     path('api/', include('core.urls')),
     path('', include('frontend.urls')),
 ]
