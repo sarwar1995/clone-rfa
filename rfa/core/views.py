3c986501b7807ec3fe657fd4a99b33b857d4594d
Sarwar Hussain <sarwar.hussain@yale.edu>
2021-03-24 11:38:12 -0400

    [WIP]: Added comments component

--- rfa/core/views.py
+++ rfa/core/views.py
@@ -41,6 +41,7 @@ class CustomUserCreate(APIView):
 
     def post(self, request, format='json'):
         serializer = CustomUserSerializer(data=request.data)
+        print(request.data)
         if serializer.is_valid():
             to_email = serializer.validated_data['email']
             # This saves the user in the database with the attribute is_active=False (default)
@@ -54,8 +55,8 @@ class CustomUserCreate(APIView):
             relative_link = reverse('verify_email')
             absolute_url = "http://" + current_site + relative_link + \
                 "?token=" + token + "&uidb64=" + url_safe_uid
-            email_body = 'Hello! ' + user.username + \
-                'Thanks for signing up on Research For All (RFA). Click the link below to verify your account.\n' + absolute_url
+            email_body = 'Hello! ' + user.username + '\n'\
+                'Thanks for signing up on Research For All (RFA). Click the link below to verify your account.\n This link is only valid for a day: ' + absolute_url
             email_data = {'subject': 'Verify your Research For All (RFA) account',
                           'email_body': email_body,
                           'to_email': to_email,
