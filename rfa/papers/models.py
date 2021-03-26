f53adfaa6d3a4452a87e83ec562e6de3cdd27ce4
Daniel Ortega <49536646+danieldortega77@users.noreply.github.com>
2021-03-26 14:55:12 -0400

    Make paper fields nullable to allow migration

--- rfa/papers/models.py
+++ rfa/papers/models.py
@@ -6,11 +6,11 @@ class Paper(models.Model):
     The paper model that contains information about doi, title, authors, date_published, journal name and abstract.
     The authors field is made a JSONfield because we can have a list of authors.
     """
-    DOI = models.CharField(max_length=200)
+    DOI = models.CharField(max_length=200, null=True)
     title = models.CharField(max_length=200)
     authors = models.JSONField()
     date_published = models.DateTimeField(auto_now_add=True)
-    journal = models.CharField(max_length=500)
+    journal = models.CharField(max_length=500, null=True)
     abstract = models.TextField()
 
 
