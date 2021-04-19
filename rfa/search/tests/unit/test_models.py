0cf7d48412d36ceb141f0b62a08c421842f27062
Robert Miranda <robertmiranda00@gmail.com>
2021-04-19 12:54:13 -0700

    Basic Search Tests
    Testing basic search functionality, including
    performing a general search, and searching for a specific
    title and DOI. Also testing searching without a
    search_term, which results in an error.

--- rfa/search/tests/unit/test_models.py
+++
@@ -1,5 +0,0 @@
-import pytest
-import json
-from django.contrib.auth import get_user_model
-from django.apps import apps
-# from search.tests.factories import UserFactory
---
+++ rfa/search/tests/unit/test_views.py
@@ -0,0 +1,69 @@
+import pytest
+import json
+from django.apps import apps
+
+from django.http import HttpRequest
+from django.test import Client
+
+from search.views import SearchPaperView
+
+S = SearchPaperView()
+request = HttpRequest()
+
+@pytest.mark.django_db
+def test_general_search():
+	request.query_params = {'search_term' : 'compilers', 'max_results' : 3}
+
+	response = S.get(request)
+	assert response.status_code == 200
+	assert len(response.data) > 1
+
+	assert 'DOI' in response.data[0].keys()
+	assert 'title' in response.data[0].keys()
+	assert 'authors' in response.data[0].keys()
+
+@pytest.mark.django_db
+def test_specific_paper_search():
+	request.query_params = {'search_term' : 'The motivic zeta functions of a matroid Jensen Kutler Usatine', 'max_results' : 3}
+
+	response = S.get(request)
+	assert response.status_code == 200
+	assert len(response.data) > 1
+
+	paper = response.data[0]
+
+	assert paper['DOI'] == '10.1112/jlms.12386'
+	assert paper['title'] == 'The motivic zeta functions of a matroid'
+	assert paper['authors'] == ['David Jensen', 'Max Kutler', 'Jeremy Usatine']
+
+@pytest.mark.django_db
+def test_doi_search():
+	request.query_params = {'search_term' : 'jlms.12386', 'max_results' : 3}
+
+	response = S.get(request)
+	assert response.status_code == 200
+	assert len(response.data) > 1
+
+	papers = response.data
+
+	DOIs = []
+	titles = []
+	authors = []
+
+	for paper in papers:
+		DOIs.append(paper['DOI'])
+		titles.append(paper['title'])
+		authors.append(paper['authors'])
+
+	assert '10.1112/jlms.12386' in DOIs
+	assert 'The motivic zeta functions of a matroid' in titles
+	assert ['David Jensen', 'Max Kutler', 'Jeremy Usatine'] in authors
+
+@pytest.mark.django_db
+def test_empty_search():
+	request.query_params = {'max_results' : 3}
+
+	with pytest.raises(KeyError):
+		response = S.get(request)
+
+
